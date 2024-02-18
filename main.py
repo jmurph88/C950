bd7f3670e0f638f7ec3dc4ea7a3be85c4a43b391
jmurphy88 <jmrigg02@gmail.com>
2024-02-18 12:03:05 -0500

    Created Distance class and load_distance_data to read distance data to list. Created Address class and a dictionary to map addresses to indices.

--- main.py
+++ main.py
@@ -2,8 +2,18 @@
 # Student ID: 001532028
 # Reference: zyBooks: Figure 7.8.2: Hash table using chaining
 # Reference: zyBooks: Figure 3.3.1: MakeChange greedy algorithm
-
+# https://github.com/FallicoFunctions/Python-Mail-Delivery-Service-Algorithm_WGU-C950 - Remove this before submittal!!!!
 import csv
+
+# Read file with distances
+with open('distanceCSV.csv') as csvfile:
+    Distance_Only = csv.reader(csvfile)
+    Distance_Only = list(Distance_Only)
+
+# Read file with address information
+with open('addressCSV.csv') as csvfile1:
+    Address_Only = csv.reader(csvfile1)
+    Address_Only = list(Address_Only)
 
 
 # HashTable class using chaining.
@@ -63,9 +73,10 @@ class ChainingHashTable:
             bucket_list.remove(key)
 
 
+# Create package object
 class Package:
-    def __init__(self, id, address, city, zipcode, deadline, weight, notes, status):
-        self.id = id
+    def __init__(self, package_id, address, city, zipcode, deadline, weight, notes, status):
+        self.package_id = package_id
         self.address = address
         self.city = city
         self.zipcode = zipcode
@@ -75,12 +86,28 @@ class Package:
         self.status = status
 
     def __str__(self):  # Overwrite
-        return "%s, %s, %s, %s, %s, %s, %s" % (self.id, self.address, self.city, self.zipcode,
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.package_id, self.address, self.city, self.zipcode,
                                                self.deadline, self.weight, self.status)
+
+
+class Truck:
+    def __init__(self, capacity, speed, load, packages, mileage, address, departure):
+        self.capacity = capacity
+        self.speed = speed
+        self.load = load
+        self.packages = packages
+        self.mileage = mileage
+        self.address = address
+        self.departure = departure
+
+    def __str__(self):  # overwrite
+        return "%s, %s, %s,  %s, %s, %s, %s," % (self.capacity, self.speed, self.load, self.packages, self.mileage,
+                                                 self.address, self.departure)
 
 
 # Load pacakge data from csv file into hash table
 def load_package_data(file_name):
+    # Read package from the CSV file
     with open(file_name) as WGUPSPackageFile:
         package_data = csv.reader(WGUPSPackageFile, delimiter=',')
         next(package_data)  # skips header
@@ -97,44 +124,82 @@ def load_package_data(file_name):
             # Create the package object
             package = Package(p_id, p_address, p_city, p_zipcode, p_deadline, p_weight, p_notes, p_status)
 
-            # insert package data into hash table
+            # insert package data into hash table using key and value (package)
             myHash.insert(p_id, package)
 
 
 # Create instance of hash table to load data
 myHash = ChainingHashTable()
 
-load_package_data('WGUPSPackageFile.csv')
+load_package_data('packageCSV.csv')
 
 
-class Distances:
-    def __init__(self, row):
-        self.row = row
+# Find the distance between two addresses
+def distance_between(row_index, column_index):
+    distance = Distance_Only[row_index][column_index]
+    if distance == '':
+        distance = Distance_Only[row_index][column_index]
 
-    # Load data into list from excel file
-    def load_distance_data(file_name):
-        with open(file_name) as distancesOnly:
-            distance_data = csv.reader(distancesOnly, delimiter=',')
-            # define dictionary for storing distance information
-            distances = []
-            # Append distance row by row to distances list
-            for row in distance_data:
-                distances.append(row)
+    return float(distance)
 
 
-# Create Address class - get addresses from file and create key value pairs in dict mapping strings to integer indices.
-class Address:
-     def address_dict(file_name):
-        with open(file_name) as distanceTable:
-            address_data = csv.reader(distanceTable)
-            headers = next(address_data)  # read the addresses from headers
+truck1 = Truck(
+    capacity=16,
+    speed=18,
+    load=0,
+    packages=[1, 2, 13, 14, 15, 16, 19, 20],  # loads packages based on ID
+    mileage=0.0,
+    address="4001 South 700 East",  # All start at the HUB
+    departure="8:00 AM"  # Need to figure out how to track time - can't be a string...datetime thing?
+)
+truck2 = Truck(
+    capacity=16,
+    speed=18,
+    load=0,
+    packages=[3, 4, 5, 9, 18, 36, 38],  # loads packages based on ID
+    mileage=0.0,
+    address="4001 South 700 East",  # All start at the HUB
+    departure="10:20 AM"
+)
 
-            address_dict = {}
-            index = 0
-            # iterate through addresses and assign to an index value
-            for address in headers:
-                address_dict[address] = index
-                index += 1
+truck3 = Truck(
+    capacity=16,
+    speed=18,
+    load=0,
+    packages=[6, 7, 8, 25, 28, 32],  # loads packages based on ID
+    mileage=0.0,
+    address="4001 South 700 East",  # All start at the HUB
+    departure="9:05 AM"
+)
 
-        return address_dict
+# Function to provide float distance between two addresses
 
+# Nearest neighbor algorithm to load the trucks based on addresses
+def deliver_packages(truck):
+    # All packages at the Hub are placed in an array.
+    at_hub = []
+    # Any package that is at the hub is added to the array
+    for package_id in truck.package:
+        package = myHash.search(package_id)
+        at_hub.append(package)
+    # Clear list so packages can be placed in order of closest address(nearest neighbor)
+    truck.pacakges.clear()
+
+    while len(at_hub) > 0:
+        next_address = float('inf')  # initialized with large value for address
+        next_package = None
+
+        # Iterate over packages that are still at hub and not delivered yet
+        for package in at_hub:
+            # Calculates distance between truck's current location and package's address
+            current_package_distance = distance_between(truck.address, package.address)
+            # Determine if current package is closer than the next package in line and update next_address and
+            # next_package
+            if current_package_distance <= next_address:
+                next_address = current_package_distance
+                next_package = package
+        # Add the next closest package to the truck's list of packages
+        truck.packages.append(next_package.package_id)
+        # Remove this package from the list of packages at the hub
+        at_hub.remove(next_package)
+    # How to track milage?
